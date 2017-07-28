70a14ac4efa2e4e5005ffee72498114d4680438d
isarceaux <isarceaux>
2017-07-28 14:09:45 +0200

    Validations added to the Corsaire model

--- app/models/corsaire.rb
+++ app/models/corsaire.rb
@@ -1,2 +1,6 @@
 class Corsaire < ApplicationRecord
+  validates :first_name, presence: true, length: { maximum: 120 }
+  validates :age, presence: true, inclusion: { in: 15...120 }
+  validates :slack_handle, format: { without: /\s/ }
+  validates :github_handle, format: { without: /\s/ }
 end
